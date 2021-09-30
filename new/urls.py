c9c971c1bfeb5f4b19f175bb0cf64a2a980563da
kjmjaemin8366 <hse07003@naver.com>
2021-09-30 22:28:26 +0900

    Before movie_id

---
+++ new/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('list/', views.list, name='list'),
+    path('upcoming/', views.Uplist, name='Uplist'),
+    # path('create/', views.create, name='movie-create'),
+    # path('create2/', views.Upcreate, name='movie-create2'),
+    #
+    # path('movie/<int:id>/', views.detail, name='movie-detail'),
+    # path('review/list/', views.review_list, name='review-list'),
+    # path('movie/<int:movie_id>/review/create/', views.review_create, name='review-create'),
+    # path('movie/<int:movie_id>/review/delete/<int:review_id>', views.review_delete, name='review-delete'),
+]
